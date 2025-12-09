a0e7de9182b04505f97073b7a778b9024ed1fbbe
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-09 14:55:53 -0400

    feat: Add profile pic to es/index.html

--- es/index.js
+++ es/index.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer')
-const fs = require('fs')
 const path = require('path')
 
 ;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  const html = fs.readFileSync(path.join(__dirname, '../es/index.html'), 'utf8')
-  await page.setContent(html, { waitUntil: 'networkidle0' })
+  const htmlPath = path.join(__dirname, '../es/index.html')
+
+  await page.goto(`file://${htmlPath}`, { waitUntil: 'networkidle0' })
 
   await page.pdf({
     path: 'manuel-morales-resume-es.pdf',
